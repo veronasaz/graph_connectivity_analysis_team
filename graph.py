9ebc3e424a4a21f46d6f3280658fa255b30011f3
veronasaz <147612156+veronasaz@users.noreply.github.com>
2023-12-03 23:54:41 +0200

    Create graph.py

---
+++ graph.py
@@ -0,0 +1,37 @@
+'''Graph'''
+
+def read_csv(file_name: str):
+    '''
+    Read graph from csv file and write it to another file.
+    '''
+    pass
+
+def graph_from_csv(file_name: str) -> dict:
+    '''
+    Read graph from csv file and return it as a dictionary.
+    '''
+    pass
+
+def components_of_connectivity(graph: dict) -> list:
+    '''
+    Finds the connectivity components of an unoriented graph and returns a list of them.
+    '''
+    pass
+
+def strong_connectivity(graph: dict) -> list:
+    '''
+    Finds the components of the strong connectivity
+    of an oriented graph and returns a list of them.
+    '''
+
+def connection_points(graph: dict) -> list:
+    '''
+    Finds the connection points of an unoriented graph and returns a list of them.
+    '''
+    pass
+
+def bridges(graph: dict) -> list:
+    '''
+    Finds the bridges of an unoriented graph and returns a list of them.
+    '''
+    pass
