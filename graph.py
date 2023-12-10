0d70a1d471b4a90df9d40078219737f896b01282
solevych <149370402+solevych@users.noreply.github.com>
2023-12-10 14:52:55 +0200

    bridges function

--- graph.py
+++ graph.py
@@ -50,15 +50,76 @@ def connection_points(graph: dict) -> list:
     '''
     pass
 
+import copy
+
 def bridges(graph: dict) -> list:
     '''
     Finds the bridges of an unoriented graph and returns a list of them.
+
     >>> bridges({0: [1], 1: [0, 2], 2: [1, 3, 4], 3: [2], \
 4: [2, 5, 6, 7], 5: [4, 6], 6: [4, 5], 7: [4, 8], 8: [7]})
     There is a list of bridges of the graph:
-    [[0, 1], [2, 3], [2, 4], [4, 5], [4, 6], [4, 7], [7, 8]]
+    [(0, 1), (1, 2), (2, 3), (2, 4), (4, 7), (7, 8)]
+
+    >>> bridges({1: [5, 2], 2: [1, 4, 5], 4: [2, 6, 7], 5: [1, 2], 6: [4, 7], 7: [3, 4, 6], 3: [7]})
+    There is a list of bridges of the graph:
+    [(2, 4), (3, 7)]
+    >>> bridges({1: [5, 2], 2: [1, 4, 5], 4: [2, 6, 7], 5: [1, 2], 6: [4, 7, 8], 7: [3, 4, 6], \
+3: [7], 8:{6, 9, 10}, 9:{8, 10, 11}, 10:{8, 9}, 11:{9}})
+    There is a list of bridges of the graph:
+    [(2, 4), (3, 7), (6, 8), (9, 11)]
     '''
-    pass
+    graph_origin=copy.deepcopy(graph)
+    edges = set()
+    for vertex, neighbors in graph.items():
+        for neighbor in neighbors:
+            edge = tuple(sorted([vertex, neighbor]))
+            edges.add(edge)
+    edges = list(edges)
+    bridges_result=[]
+
+    def dfs(graph):
+        """
+        dfs to look if graph is connected 
+        """
+        step=[]
+        dfs_result=[]
+        vertex=min(graph)
+
+        while len(dfs_result)<len(graph):
+            if vertex not in dfs_result:
+                dfs_result.append(vertex)
+                step.append(vertex)
+
+            count=0
+            for ver in sorted(graph[vertex]):
+                if ver not in dfs_result:
+                    vertex=ver
+                    count+=1
+                    break
+
+            if count==0:
+                if len(step)>1:
+                    step.remove(step[-1])
+                    vertex=step[-1]
+                elif len(step)==1 and len(dfs_result)<len(graph):
+                    return False
+
+        return True
+
+    for edge in edges:
+        graph[edge[0]].remove(edge[1])
+        graph[edge[1]].remove(edge[0])
+        if dfs(graph) is False:
+            bridges_result.append(edge)
+        graph=copy.deepcopy(graph_origin)
+    
+    if bridges_result:
+        bridges_sorted = sorted(bridges_result, key=lambda bridge: bridge[0])
+        bridges_sorted = sorted(bridges_result, key=lambda bridge: bridge[1])
+        print('There is a list of bridges of the graph:')
+        return bridges_sorted
+    return 'There are no bridges in the graph.'
 
 if __name__ == '__main__':
     import doctest
